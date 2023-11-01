2a80b20fd0cf6d90b44afc61c3f605fe8620285a
Thiesa <thiesa.c@gmail.com>
2023-11-01 12:04:00 -0400

    req3 still not working

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,42 +1,30 @@
-from file_management import txt_importer
-
-
 def process(path_file, instance):
     """Processa o arquivo e o adiciona à fila, se ainda não estiver presente"""
-    if path_file in [item["nome_do_arquivo"] for item in instance.items]:
-        print(f"Arquivo {path_file} já processado, ignorando.")
+    if path_file in [item["nome_do_arquivo"] for item in instance._data]:
+        return
     else:
-        data = txt_importer(path_file)
-        instance.enqueue(
-            {"nome_do_arquivo": path_file, "linhas_do_arquivo": data}
-        )
-        print(
-            f'{{\n "nome_do_arquivo": "{path_file}",\n'
-            f'    "qtd_linhas": {len(data)},\n'
-            f'    "linhas_do_arquivo": {data}\n}}'
-        )
+        with open(path_file, "r") as file:
+            data = [line.strip() for line in file]
+            instance.enqueue(
+                {"nome_do_arquivo": path_file, "linhas_do_arquivo": data}
+            )
 
 
 def remove(instance):
     """Remove o primeiro elemento da fila"""
-    try:
-        removed_item = instance.dequeue()
+    removed_item = instance.dequeue()
+    if removed_item:
         print(
             f"Arquivo {removed_item['nome_do_arquivo']} removido com sucesso."
         )
-    except IndexError:
-        print("Não há elementos na fila, não é possível remover.")
 
 
 def file_metadata(instance, position):
     """Obtém metadados do arquivo na posição especificada na fila"""
-    try:
-        file_data = instance.search(position)
-        metadata = {
-            "nome_do_arquivo": file_data["nome_do_arquivo"],
-            "qtd_linhas": len(file_data["linhas_do_arquivo"]),
-            "linhas_do_arquivo": file_data["linhas_do_arquivo"],
-        }
-        return metadata
-    except IndexError:
-        raise IndexError("Posição inválida na fila")
+    file_data = instance.search(position)
+    metadata = {
+        "nome_do_arquivo": file_data["nome_do_arquivo"],
+        "qtd_linhas": len(file_data["linhas_do_arquivo"]),
+        "linhas_do_arquivo": file_data["linhas_do_arquivo"],
+    }
+    return metadata

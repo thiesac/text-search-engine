25d61f22e5ea6945d4b48405dc311743094ce455
Thiesa <thiesa.c@gmail.com>
2023-11-01 08:42:47 -0400

    feat - Req2: file_management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.isfile(path_file):
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    elif not path_file.endswith("txt"):
+        print("Formato inválido", file=sys.stderr)
+    else:
+        with open(path_file, "r") as file:
+            content = file.read()
+            piece_of_news = content.split("\n")
+            return piece_of_news

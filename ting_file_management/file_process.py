b142cc4775b45a592e6d34cfa8069d701e5c817d
Thiesa <thiesa.c@gmail.com>
2023-11-01 09:52:53 -0400

    feat: Req3 file_process - test not working

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,42 @@
+from file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    """Processa o arquivo e o adiciona à fila, se ainda não estiver presente"""
+    if path_file in [item["nome_do_arquivo"] for item in instance.items]:
+        print(f"Arquivo {path_file} já processado, ignorando.")
+    else:
+        data = txt_importer(path_file)
+        instance.enqueue(
+            {"nome_do_arquivo": path_file, "linhas_do_arquivo": data}
+        )
+        print(
+            f'{{\n "nome_do_arquivo": "{path_file}",\n'
+            f'    "qtd_linhas": {len(data)},\n'
+            f'    "linhas_do_arquivo": {data}\n}}'
+        )
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    """Remove o primeiro elemento da fila"""
+    try:
+        removed_item = instance.dequeue()
+        print(
+            f"Arquivo {removed_item['nome_do_arquivo']} removido com sucesso."
+        )
+    except IndexError:
+        print("Não há elementos na fila, não é possível remover.")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    """Obtém metadados do arquivo na posição especificada na fila"""
+    try:
+        file_data = instance.search(position)
+        metadata = {
+            "nome_do_arquivo": file_data["nome_do_arquivo"],
+            "qtd_linhas": len(file_data["linhas_do_arquivo"]),
+            "linhas_do_arquivo": file_data["linhas_do_arquivo"],
+        }
+        return metadata
+    except IndexError:
+        raise IndexError("Posição inválida na fila")
